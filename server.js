cb0e26f0015265f20ed6220da05586eeffa01142
Nirel Chua <95299412+cninz@users.noreply.github.com>
2022-06-05 11:50:08 -0600

    Modified

--- server.js
+++ server.js
@@ -2,19 +2,37 @@ const express = require('express')
 const app = express()
 const PORT = 8000
 
+// const rappers = {
+//     '21 Savage': {
+//         'age': 29,
+//         'birthName': 'Sheyaa Bin Abraham-Joseph', 
+//         'birthLocation': 'London, England'
+//     },
+//     'Chance the Rapper': {
+//         'age': 29,
+//         'birthName': 'Chancelor Bennett',
+//         'birthLocation': 'Chicago, Illinois'
+//     },
+//     'unknown':{
+//         'age':0,
+//         'birthName': 'unknown',
+//         'birthLocation': 'unknown'
+//     }
+// }
+
 const rappers = {
-    '21 Savage': {
-        'age': 29,
-        'birthName': 'Shéyaa Bin Abraham-Joseph', 
-        'birthLocation': 'London, England'
+    '21 savage': {
+        'age' : 29,
+        'birthName' : 'Shéyaa Bin Abraham-Joseph', 
+        'birthLocation' : 'London, England'
     },
-    'Chance the Rapper': {
-        'age': 29,
-        'birthName': 'Chancelor Bennett',
+    'chance the rapper':{
+        'age' : 29,
+        'birthName' : 'Chancelor Bennett',
         'birthLocation': 'Chicago, Illinois'
     },
     'unknown':{
-        'age':0,
+        'age': 0,
         'birthName': 'unknown',
         'birthLocation': 'unknown'
     }
@@ -25,13 +43,14 @@ app.get('/', (request, response) => {
     response.sendFile(__dirname + '/index.html')
 })
 
-app.get('/api/:name', (request, response) => {
+app.get('/api/:name', (request,response) =>{
     const rapperName = request.params.name.toLowerCase()
-    if (rappers [rapperName]){
+    if (rappers[rapperName]){
         response.json(rappers[rapperName])
     }else{
         response.json(rappers['unknown'])
     }
+    
 })
 
 app.listen(PORT, () => {
